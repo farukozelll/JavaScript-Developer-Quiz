cbfed0dd761f5e28bdabcc6b4df5aa503cbeec60
Faruk <47918693+farukozelll@users.noreply.github.com>
2020-09-30 09:33:34 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,13 @@
+# JavaScript Quiz App
+
+ JavaScript Quiz App
+version: 1.0.0
+
+## Deployment
+
+[Ziyaret et Javascript-Quiz-App](https://ozelfaruk.com/)
+
+## Authors
+
+* **Faruk Özel** - [Github](https://github.com/farukozelll)
+
---
+++ data.js
@@ -0,0 +1,100 @@
+/*
+ * JavaScript quiz app
+ * version: 1.0.0
+ * By: Faruk Özel
+ * date: Eylül 28 2020
+ */
+ 
+var data = [
+  {
+    id: 1,
+    question: "HTML ne anlama geliyor?",
+    a: "Köprüler ve Metin Biçimlendirme Dili",
+    b: "Ev Aracı Biçimlendirme Dili",
+    c: "Hiper Metin Biçimlendirme Dili",
+    d: "Hiper Metin Java Dili",
+    answer: 3
+  },
+  {
+    id: 2,
+    question: "Programdaki bir hata veya ekipmandaki bir kusurdan kaynaklanan bir arıza.",
+    a: "Bug",
+    b: "Byte",
+    c: "Tarayıcı",
+    d: "Crash",
+    answer: 1
+  },
+  {
+    id: 3,
+    question: "Slang. Ekipman kusurlarını bulmak ve düzeltmek veya arızaları programlamak için.",
+    a: "Default",
+    b: "Byte",
+    c: "Debug",
+    d: "DSL",
+    answer: 3
+  },
+  {
+    id: 4,
+    question: "CSS ne anlama geliyor?",
+    a: "Basamaklı Stil Sayfaları",
+    b: "Renkli Stil Sayfaları",
+    c: "Yaratıcı Stil Sayfaları",
+    d: "Bilgisayar Stil Sayfaları",
+    answer: 1
+  },
+  {
+    id: 5,
+    question: "Tüm dosyaların bilgisayarda tutulduğu bir depo.",
+    a: "Disk",
+    b: "Directory",
+    c: "Domain",
+    d: "DVD",
+    answer: 2
+  },
+  {
+    id: 6,
+    question: "Bilgisayarda depolanan bir dizi veri.",
+    a: "File",
+    b: "Byte",
+    c: "Ethernet",
+    d: "Şifreleme",
+    answer: 1
+  },
+
+  {
+    id: 7,
+    question: "Birbirine bağlı bilgisayarlar sistemi.",
+    a: "Programlama dili",
+    b: "PC",
+    c: "Rasgele Erişim Belleği",
+    d: "Network",
+    answer: 4
+  },
+  {
+    id: 8,
+    question: "PHP ne anlama geliyor?",
+    a: "Kişisel Köprü Metni İşlemcisi",
+    b: " Özel Ana Sayfa",
+    c: "İşlemci Nasıl Yazdırılır",
+    d: "Hypertext Ön İşlemcisi",
+    answer: 4
+  },
+  {
+    id: 9,
+    question: "Birçok üst düzey bilgisayar uygulamasının temeli olarak kullanılan çok güçlü bir işletim sistemi.",
+    a: "Windows",
+    b: "UNIX",
+    c: "HS-OS",
+    d: "CPU",
+    answer: 2
+  },
+  {
+    id: 10,
+    question: "Hypertext Transfer Protocol Secure, genellikle şirket içi internet sitelerinde kullanılır. Erişim sağlamak için şifreler gereklidir.",
+    a: "SSL",
+    b: "FTP",
+    c: "HTTPS",
+    d: "HTTP",
+    answer: 3
+  }
+];
